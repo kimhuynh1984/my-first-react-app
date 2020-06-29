3b56811c3f202e5f326732a4e20ddd192260e218
kimhuynh1984 <kimhuynh1984@gmail.com>
2020-06-29 19:11:27 +0200

    App.js

---
+++ src/App.js
@@ -0,0 +1,31 @@
+// src/App.js
+// ...
+import Title from "./components/Title";
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <Title />
+      </main>
+
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+// ...
